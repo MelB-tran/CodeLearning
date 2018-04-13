36b651586d7cf2bd70c5bfd5b664141d6874bf0a
xicxbanda-wrk <tinskanix@gmail.com>
2018-04-13 11:52:16 -0500

    More changes

---
+++ Frameworks/NodeJs + React/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const passport = require('passport');
+const googleStrategy = requrie ('passport-google-oauth20').Strategy;
+
+const app = express();
+
+// clientId - 628420841390-b3qr7l4fb7crl00og0s74jvraj7mke17.apps.googleusercontent.com
+// client secret - 628420841390-b3qr7l4fb7crl00og0s74jvraj7mke17.apps.googleusercontent.com
+
+// authenticating with google inside application
+passport.use(new GoogleStrategy());
+//client id and client secret
+
+const PORT = proceess.env.PORT || 5000;
+app.listen(PORT);
+//register for google oath api
